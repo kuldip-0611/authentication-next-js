d133e6d58ffed185aef7c34021821b4b74dd26d5
kuldip-zignuts <kuldipp@zignuts.com>
2023-03-30 15:13:10 +0530

    secure pages in server side

--- pages/blog.js
+++ pages/blog.js
@@ -11,6 +11,15 @@ export default blog
 export async function getServerSideProps(context){
     const session = getSession(context);
 
+    if(session === null){
+        return{
+            redirect:{
+                destination:"/api/auth/signin?callbackUrl=http://localhost:3000/blog",
+                permanent:false
+            }
+        }
+    }
+
     return{
         props:{
             data:session ? 'bharelu' : 'khali'
