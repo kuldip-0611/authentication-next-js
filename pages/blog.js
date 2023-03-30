6d7d6aee6a32e5e7eb7fe86cb5286b99a7b548b9
kuldip-zignuts <kuldipp@zignuts.com>
2023-03-30 15:05:47 +0530

    server side authentication

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,20 @@
-const blog = ()=>{
+import { getSession } from "next-auth/react"
+
+const blog = ({data})=>{
+
     return(
-        <h1>blog Page</h1>
+        <h1>blog-page === {data}</h1>
     )
 }
 export default blog
+
+export async function getServerSideProps(context){
+    const session = getSession(context);
+
+    return{
+        props:{
+            data:session ? 'bharelu' : 'khali'
+        }
+    }
+
+}
