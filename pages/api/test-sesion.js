e7195ec99a3a7c0b3da44e7dee288092ad7b0859
kuldip-zignuts <kuldipp@zignuts.com>
2023-03-30 15:43:34 +0530

    secure api routes

---
+++ pages/api/test-sesion.js
@@ -0,0 +1,12 @@
+import { getSession } from "next-auth/react"
+const handler =  async (req,res)=>{
+    const session = await getSession({req})
+    if(session){
+        return res.status(200).json({message:'success'},session)
+    }else{
+        return res.status(401).json({message:"Unauthorized"})
+    }
+
+}
+
+export default handler
